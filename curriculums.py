9bcb63108c39714d3be668b34db9c4e6026168a7
edwardl <edwardl@v.cluster.local>
2021-12-27 23:40:41 -0500

    change CARLA training curriculum

--- curriculums.py
+++ curriculums.py
@@ -119,9 +119,10 @@ CelebA = {
 
 CARLA = {
     0: {'batch_size': 10, 'num_steps': 48, 'img_size': 32, 'batch_split': 1, 'gen_lr': 4e-5, 'disc_lr': 4e-4},
-    int(10e3): {'batch_size': 14, 'num_steps': 48, 'img_size': 64, 'batch_split': 2, 'gen_lr': 2e-5, 'disc_lr': 2e-4},
-    int(55e3): {'batch_size': 10, 'num_steps': 48, 'img_size': 128, 'batch_split': 5, 'gen_lr': 10e-6, 'disc_lr': 10e-5},
-    int(200e3): {},
+    int(50e3): {'batch_size': 5, 'num_steps': 48, 'img_size': 64, 'batch_split': 2, 'gen_lr': 2e-5, 'disc_lr': 2e-4},
+    int(500e3): {},
+    # int(55e3): {'batch_size': 1, 'num_steps': 48, 'img_size': 128, 'batch_split': 5, 'gen_lr': 10e-6, 'disc_lr': 10e-5},
+    # int(200e3): {},
 
     'dataset_path': '/h/edwardl/datasets/carla/images/*.png',
     'fov': 30,
