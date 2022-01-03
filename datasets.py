8764188c96aabdde2d90861070e060c4adff9e4e
Edward Luo <edward.luo@mail.utoronto.ca>
2022-01-02 17:05:35 -0800

    Black on datasets.pu

--- datasets.py
+++ datasets.py
@@ -22,9 +22,19 @@ class CelebA(Dataset):
         super().__init__()
 
         self.data = glob.glob(dataset_path)
-        assert len(self.data) > 0, "Can't find data; make sure you specify the path to your dataset"
+        assert (
+            len(self.data) > 0
+        ), "Can't find data; make sure you specify the path to your dataset"
         self.transform = transforms.Compose(
-                    [transforms.Resize(320), transforms.CenterCrop(256), transforms.ToTensor(), transforms.Normalize([0.5], [0.5]), transforms.RandomHorizontalFlip(p=0.5), transforms.Resize((img_size, img_size), interpolation=0)])
+            [
+                transforms.Resize(320),
+                transforms.CenterCrop(256),
+                transforms.ToTensor(),
+                transforms.Normalize([0.5], [0.5]),
+                transforms.RandomHorizontalFlip(p=0.5),
+                transforms.Resize((img_size, img_size), interpolation=0),
+            ]
+        )
 
     def __len__(self):
         return len(self.data)
@@ -34,65 +44,95 @@ class CelebA(Dataset):
         X = self.transform(X)
 
         return X, 0
+
 
 class Cats(Dataset):
     """Cats Dataset"""
 
     def __init__(self, dataset_path, img_size, **kwargs):
         super().__init__()
-        
+
         self.data = glob.glob(dataset_path)
-        assert len(self.data) > 0, "Can't find data; make sure you specify the path to your dataset"
+        assert (
+            len(self.data) > 0
+        ), "Can't find data; make sure you specify the path to your dataset"
         self.transform = transforms.Compose(
-                    [transforms.Resize((img_size, img_size), interpolation=0), transforms.ToTensor(), transforms.Normalize([0.5], [0.5]), transforms.RandomHorizontalFlip(p=0.5)])
+            [
+                transforms.Resize((img_size, img_size), interpolation=0),
+                transforms.ToTensor(),
+                transforms.Normalize([0.5], [0.5]),
+                transforms.RandomHorizontalFlip(p=0.5),
+            ]
+        )
+
     def __len__(self):
         return len(self.data)
-    
+
     def __getitem__(self, index):
         X = PIL.Image.open(self.data[index])
         X = self.transform(X)
-        
+
         return X, 0
+
 
 class Carla(Dataset):
     """Carla Dataset"""
 
     def __init__(self, dataset_path, img_size, **kwargs):
         super().__init__()
-        
+
         self.data = glob.glob(dataset_path)
-        assert len(self.data) > 0, "Can't find data; make sure you specify the path to your dataset"
+        assert (
+            len(self.data) > 0
+        ), "Can't find data; make sure you specify the path to your dataset"
         self.transform = transforms.Compose(
-                    [transforms.Resize((img_size, img_size), interpolation=0), transforms.ToTensor(), transforms.Normalize([0.5], [0.5])])
+            [
+                transforms.Resize((img_size, img_size), interpolation=0),
+                transforms.ToTensor(),
+                transforms.Normalize([0.5], [0.5]),
+            ]
+        )
 
     def __len__(self):
         return len(self.data)
-    
+
     def __getitem__(self, index):
         X = PIL.Image.open(self.data[index])
         X = self.transform(X)
-        
+
         return X, 0
 
+
 class ShapeNetCars(Dataset):
-    """ ShapeNet Cars Dataset"""
+    """ShapeNet Cars Dataset"""
 
     def __init__(self, dataset_path, img_size, **kwargs):
         super().__init__()
-        
+
         self.data = glob.glob(dataset_path)
-        assert len(self.data) > 0, "Can't find data; make sure you specify the path to your dataset"
+        assert (
+            len(self.data) > 0
+        ), "Can't find data; make sure you specify the path to your dataset"
         self.transform = transforms.Compose(
-                    [transforms.Resize((img_size, img_size), interpolation=torchvision.transforms.InterpolationMode.BILINEAR), transforms.ToTensor(), transforms.Normalize([0.5], [0.5])])
+            [
+                transforms.Resize(
+                    (img_size, img_size),
+                    interpolation=torchvision.transforms.InterpolationMode.BILINEAR,
+                ),
+                transforms.ToTensor(),
+                transforms.Normalize([0.5], [0.5]),
+            ]
+        )
 
     def __len__(self):
         return len(self.data)
-    
+
     def __getitem__(self, index):
         X = PIL.Image.open(self.data[index])
         X = self.transform(X)
-        
+
         return X, 0
+
 
 def get_dataset(name, subsample=None, batch_size=1, **kwargs):
     dataset = globals()[name](**kwargs)
@@ -103,9 +143,10 @@ def get_dataset(name, subsample=None, batch_size=1, **kwargs):
         shuffle=True,
         drop_last=True,
         pin_memory=False,
-        num_workers=8
+        num_workers=8,
     )
     return dataloader, 3
+
 
 def get_dataset_distributed(name, world_size, rank, batch_size, **kwargs):
     dataset = globals()[name](**kwargs)
