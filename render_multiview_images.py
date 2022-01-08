81bbe67c2946c0abebe73e7ea17617573d92dd18
Edward Luo <edward.luo@mail.utoronto.ca>
2022-01-07 22:10:01 -0800

    fix generator scope

--- render_multiview_images.py
+++ render_multiview_images.py
@@ -24,7 +24,7 @@ def show(tensor_img):
 def generate_img(gen, z, **kwargs):
     
     with torch.no_grad():
-        img, depth_map = generator.staged_forward(z, **kwargs)
+        img, depth_map = gen.staged_forward(z, **kwargs)
         tensor_img = img.detach()
         
         img_min = img.min()
